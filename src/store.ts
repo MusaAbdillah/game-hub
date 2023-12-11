38f686808fb8d4c098a63360a33ac651daff7fd0
musa <musa.abdillah@ottodigital.id>
2023-12-11 15:40:17 +0700

    store as state with zustand

---
+++ src/store.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+export interface GameQuery {
+    GenreId?: number;
+    PlatformId?: number;
+    Ordering?: string;
+    SearchText?: string;
+  }
+
+interface GameQueryStore {
+    gameQuery: GameQuery;
+    setGenreId: (genreId: number) => void;
+    setPlatformId: (platformId: number) => void;
+    setSortOrder: (sortOrder: string) => void;
+    setSearchText: (searchText: string) => void;
+}
+
+
+create<GameQueryStore>((set) => ({
+    gameQuery: {},
+    setSearchText: (searchText) => set((store) => ({ gameQuery: { ...store.gameQuery, searchText }})),
+    setGenreId: (genreId) => set((store)  => ({ gameQuery: { ...store.gameQuery, genreId}})),
+    setPlatformId: (platformId) => set((store)  => ({ gameQuery: { ...store.gameQuery, platformId}})),
+    setSortOrder: (sortOrder) => set((store)  => ({ gameQuery: { ...store.gameQuery, sortOrder}}))
+}))
