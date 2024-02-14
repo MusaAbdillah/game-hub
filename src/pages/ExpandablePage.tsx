f02c5f89ed62bccada6d7e7a0f2622d611367da1
musa <musa.abdillah@ottodigital.id>
2024-02-14 10:13:59 +0700

    expandable

---
+++ src/pages/ExpandablePage.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@chakra-ui/button";
+import { Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface Props {
+    children: string;
+}
+
+const ExpandablePage = ({children}: Props) => {
+  
+    const [expanded, setExpanded] = useState(false);
+    const limit = 300;
+
+    if (children.length <= limit)
+        return <Text>{children}</Text>;
+    
+    const summary = expanded ? children : children.substring(0, limit) + "..." 
+    
+    return (
+            <Text>
+                {summary}
+                <Button
+                    size="xs"
+                    fontWeight="bold"
+                    colorScheme="yellow"
+                    marginLeft={3}
+                    onClick={() => setExpanded(!expanded)}>
+                    {expanded ? 'Show less' : 'Show more'}
+                </Button>
+            </Text>
+    )
+}
+
+export default ExpandablePage;
