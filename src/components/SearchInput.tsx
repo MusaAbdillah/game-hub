b5a5c4dd20c7fd7e3b79a152f2a5daaadb9be365
musa <musa.abdillah@ottodigital.id>
2024-02-16 14:14:10 +0700

    navigate when search on detail page

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../store";
 interface SearchInputProps {
   onSearch: (search: string) => void;
@@ -9,12 +10,14 @@ interface SearchInputProps {
 function SearchInput() {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore(s => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate("/");
       }}
     >
       <InputGroup>
